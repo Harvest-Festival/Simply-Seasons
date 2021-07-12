38e09965fde33597150c9a031382469c3f19f4be
joshiejack <joshjackwildman@gmail.com>
2021-07-13 00:45:13 +0100

    reduce bee update to frequency to once a minute, to give them a chance to actually execute their AI

--- src/main/java/uk/joshiejack/simplyseasons/mixins/SSBeeEntity.java
+++ src/main/java/uk/joshiejack/simplyseasons/mixins/SSBeeEntity.java
@@ -65,7 +65,7 @@ public abstract class SSBeeEntity extends AnimalEntity {
      **/
     @Inject(method = "tick", at = @At(value = "TAIL"))
     protected void updateGoals(CallbackInfo ci) {
-        if (!level.isClientSide && level.getDayTime() % 100 == 0
+        if (!level.isClientSide && level.getDayTime() % 1200 == 0
                 && beePollinateGoal != null && findPollinationTargetGoal != null && goToKnownFlowerGoal != null) {
             level.getCapability(SSeasonsAPI.SEASONS_CAPABILITY)
                     .ifPresent(provider -> {
