a2f24859b87a096f8f54fa986365329626ac1c22
joshiejack <joshjackwildman@gmail.com>
2024-03-14 21:32:40 +0000

    Add StringRepresentable to Season enum

--- src/main/java/uk/joshiejack/simplyseasons/api/Season.java
+++ src/main/java/uk/joshiejack/simplyseasons/api/Season.java
@@ -1,15 +1,24 @@
 package uk.joshiejack.simplyseasons.api;
 
 import com.mojang.serialization.Codec;
+import net.minecraft.util.StringRepresentable;
+import org.jetbrains.annotations.NotNull;
 
-public enum Season {
+import java.util.Locale;
+
+public enum Season implements StringRepresentable {
     SPRING, SUMMER, AUTUMN, WINTER, WET, DRY;
 
-    public static final Codec<Season> CODEC = Codec.STRING.xmap(Season::valueOf, Season::name);
+    public static final Codec<Season> CODEC = StringRepresentable.fromEnum(Season::values);
 
     public static final Season[] MAIN = new Season[4];
 
     static {
         System.arraycopy(values(), 0, MAIN, 0, 4);
+    }
+
+    @Override
+    public @NotNull String getSerializedName() {
+        return name().toLowerCase(Locale.ENGLISH);
     }
 }
